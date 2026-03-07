52bdb6903c9ee50c5acd9ed3bb5c65e5ae9fe04d
BritiJadav <britijadav@gmail.com>
2026-03-07 13:57:35 +0530

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from data_loader import load_data, train_valid_split, one_hot_encode
-
-# You can now call these functions, e.g.:
-# data = load_data()
-# train_data, valid_data = train_valid_split(data)
-# encoded_data = one_hot_encode(data)
